a2c76fc83f8457ee9dd3e45a49c1d76931956fd9
drinkingWater <aamuti64@gmail.com>
2021-12-18 21:58:27 +0600

    Create Boolean.test.js

---
+++ test/type/Boolean.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { describe, test } from 'mocha';
+import Boolean from '../../src/type/Boolean.js';
+import Type from '../../src/core/Type.js';
+
+describe('Boolean', () => {
+	test('Boolean is a instance of Type class', () => {
+		expect(Boolean).to.instanceOf(Type);
+	});
+});
