5eac40e36722655472bdd17fc2eab1b0d396da79
Noman5237 <anonyman637@gmail.com>
2021-12-19 18:31:46 +0600

    #2 #3 formatted, added doc

--- src/core/Type.js
+++ src/core/Type.js
@@ -1,13 +1,21 @@
-import IllegalArgumentException from '../exceptions/IllegalArgumentException.js';
+import IllegalArgumentError from '../error/IllegalArgumentError.js';
 
 /**
+ * <p>
  * Type System plays a crucial role in schema definition and cloning.
  *
+ * <p>
  * In schema definition when the default value of a particular type is not given
  * it is fetched from the type system. It ensures consistency and less undefined values.
  * So the default value of a newly declared type cannot be undefined.
  *
+ * <p>
  * By providing a cloning method, it facilitates deep copying custom values of newly created types.
+ * And with the newly added serialize and deserialize methods we can easily serialize and deserialize the components
+ *
+ * <p>
+ * We can't guarantee 100% runtime type safety just using the type class and that's why there are test classes which
+ * provides the additional benefits, so when you invent a new type, please compose the tests with care.
  *
  * @todo write examples and tutorials
  * @since 0.0.1
@@ -21,9 +29,19 @@ class Type {
 	 * @since 0.0.1
 	 * @constant
 	 * @private
-	 * @type {String}
+	 * @type {string}
 	 */
 	#name;
+
+	/**
+	 * Default value of the type when not initialized
+	 *
+	 * @since 0.0.1
+	 * @constant
+	 * @private
+	 * @type {any}
+	 */
+	#defaultValue;
 
 	/**
 	 * Validator function checks whether a value can be of this type.
@@ -56,7 +74,7 @@ class Type {
 	#serialize;
 
 	/**
-	 * deSerialize function which deserializes the value of this specific type
+	 * Deserialize function which deserializes the value of this specific type
 	 *
 	 * @since 0.0.1
 	 * @constant
@@ -64,16 +82,6 @@ class Type {
 	 * @type {Function}
 	 */
 	#deserialize;
-
-	/**
-	 * Default value of the type when not initialized
-	 *
-	 * @since 0.0.1
-	 * @constant
-	 * @private
-	 * @type {any}
-	 */
-	#defaultValue;
 
 	/* ================================ CONSTRUCTORS ================================ */
 
@@ -83,35 +91,46 @@ class Type {
 	 * @since 0.0.1
 	 * @public
 	 * @constructor
-	 * @param {String} name name of the type
-	 * @param {Function} clone function that handles the responsibility of cloning values of this type
-	 * @param {Function} validator function that validates type of any value against this type
-	 * @param {any} defaultValue default value of the type
-	 * @throws {IllegalArgumentException} when type of name is not string
-	 * or validator is not a function with one argument
-	 * or clone is not a function with one argument
+	 * @param {object} props props of the type object
+	 * @property {string} name name of the type
+	 * @property {any} defaultValue default value of the type
+	 * @property {Function} clone function that handles the responsibility of cloning values of this type
+	 * @property {Function} validator function that validates type of any value against this type
+	 * @property {Function} serialize function that serializes the value of this type
+	 * @property {Function} deserialize function that deserializes the value of this type
+	 * @throws {IllegalArgumentError} when type of name is not string
 	 * or default value is undefined
 	 * or the default value cannot be validated against provided validator
+	 * or validator is not a function with one argument
+	 * or clone is not a function with one argument
+	 * or serialize is not a function with one argument
+	 * or deserialize is not a function with one argument
 	 */
-	constructor({ name, validator, clone, defaultValue, serialize, deserialize }) {
+	constructor({ name, defaultValue, validator, clone, serialize, deserialize }) {
 		if (process.env.NODE_ENV !== 'production') {
 			if (!(typeof name === 'string')) {
-				throw new IllegalArgumentException(
-					'Cannot create new type definition. Provided type name is not a valid string.'
-				);
+				throw new IllegalArgumentError('Cannot create new type definition. Provided type name is not a valid string.');
 			} else if (!(validator instanceof Function && validator.length === 1)) {
-				throw new IllegalArgumentException(
+				throw new IllegalArgumentError(
 					'Cannot create new type definition. Provided function is not a valid validator function.'
 				);
 			} else if (!(clone instanceof Function && clone.length === 1)) {
-				throw new IllegalArgumentException(
+				throw new IllegalArgumentError(
 					'Cannot create new type definition. Provided function is not a valid clone function.'
 				);
 			} else if (defaultValue === undefined) {
-				throw new IllegalArgumentException('Cannot create new type definition. Provided default value is undefined.');
+				throw new IllegalArgumentError('Cannot create new type definition. Provided default value is undefined.');
 			} else if (validator(defaultValue) !== true) {
-				throw new IllegalArgumentException(
+				throw new IllegalArgumentError(
 					'Cannot create new type definition. Provided default value cannot be validated using provided validator.'
+				);
+			} else if (!(serialize instanceof Function && serialize.length === 1)) {
+				throw new IllegalArgumentError(
+					'Cannot create new type definition. Provided function is not a valid serialize function.'
+				);
+			} else if (!(deserialize instanceof Function && deserialize.length === 1)) {
+				throw new IllegalArgumentError(
+					'Cannot create new type definition. Provided function is not a valid deserialize function.'
 				);
 			}
 		}
@@ -133,7 +152,7 @@ class Type {
 	 * @since 0.0.1
 	 * @public
 	 * @param {any} value the value to validate against this type
-	 * @returns {Boolean} true if the provided value can be validated against this type
+	 * @returns {boolean} true if the provided value can be validated against this type
 	 */
 	validate(value) {
 		return this.#validator(value);
@@ -146,28 +165,52 @@ class Type {
 	 * @public
 	 * @param {any} value the value to clone
 	 * @returns {any} the cloned value
-	 * @throws {IllegalArgumentException} when value cannot be validated against this type
+	 * @throws {IllegalArgumentError} when value cannot be validated against this type
 	 */
 	clone(value) {
 		if (process.env.NODE_ENV !== 'production') {
 			if (!this.validate(value)) {
-				throw new IllegalArgumentException('Provided value cannot be validated against this type.');
+				throw new IllegalArgumentError('Provided value cannot be validated against this type.');
 			}
 		}
 		return this.#clone(value);
 	}
 
+	/**
+	 * Serialize the provided value using the serialize function of defined type
+	 *
+	 * @since 0.0.1
+	 * @public
+	 * @param {any} value the value to serialize 
+	 * @returns {any} the serialized value
+	 * @throws {IllegalArgumentError} when value cannot be validated against this type
+	 */
 	serialize(value) {
 		if (process.env.NODE_ENV !== 'production') {
 			if (!this.validate(value)) {
-				throw new IllegalArgumentException('Provided value cannot be validated against this type.');
+				throw new IllegalArgumentError('Provided value cannot be validated against this type.');
 			}
 		}
 		return this.#serialize(value);
 	}
 
+	/**
+	 * Deserialize the provided value using the deserialize function of defined type
+	 *
+	 * @since 0.0.1
+	 * @public
+	 * @param {any} value the value to deserialize 
+	 * @returns {any} the deserialized value
+	 * @throws {IllegalArgumentError} when deserialized value cannot be validated against this type
+	 */
 	deserialize(value) {
-		return this.#deserialize(value);
+		const deserializedValue = this.#deserialize(value);
+		if (process.env.NODE_ENV !== 'production') {
+			if (!this.validate(deserializedValue)) {
+				throw new IllegalArgumentError('Deserialized value cannot be validated against this type.');
+			}
+		}
+		return deserializedValue;
 	}
 
 	/* ================================ GETTERS ================================ */
@@ -177,7 +220,7 @@ class Type {
 	 *
 	 * @since 0.0.1
 	 * @public
-	 * @returns {String} the name of the type
+	 * @returns {string} the name of the type
 	 */
 	getName() {
 		return this.#name;
@@ -210,7 +253,7 @@ class Type {
 	 *
 	 * @since 0.0.1
 	 * @public
-	 * @returns {String} string representation
+	 * @returns {string} string representation
 	 */
 	toString() {
 		return `Type: ${this.getName()}`;
--- src/error/InterfaceError.js
+++ src/error/InterfaceError.js
@@ -1,17 +1,17 @@
 /**
  *
- * Interface Exception can be thrown when
+ * Interface Error can be thrown when
  * when users of the API broke the contract or the critical warnings
  *
  * @since 0.0.1
  * @public
  * @class
  */
-class InterfaceException extends Error {
+class InterfaceError extends Error {
 	constructor(...params) {
 		super(params);
-		this.name = 'InterfaceException';
+		this.name = 'InterfaceError';
 	}
 }
 
-export default InterfaceException;
+export default InterfaceError;
