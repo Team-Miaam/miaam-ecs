ee54b9f2390da2ef63cac94163b3b73bbddf806e
Noman5237 <anonyman637@gmail.com>
2022-01-29 00:55:43 +0600

    system class skeleton

--- src/core/System.js
+++ src/core/System.js
@@ -1,3 +1,18 @@
-class System {}
+/* eslint-disable no-empty-function */
+/* eslint-disable class-methods-use-this */
+class System {
+	// eslint-disable-next-line no-useless-constructor
+	constructor() {}
+
+	init() {}
+
+	beforeUpdate() {}
+
+	update() {}
+
+	afterUpdate() {}
+
+	destroy() {}
+}
 
 export default System;
