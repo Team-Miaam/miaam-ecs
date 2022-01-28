e83f60f65f94126c07a15ae9d15d110fbcd4155a
Noman5237 <anonyman637@gmail.com>
2022-01-29 03:26:19 +0600

    merge history complete

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import Component from './src/Component.js';
+import Component from './src/core/Component.js';
+import Entity from './src/core/Entity.js';
+import System from './src/core/System.js';
+import Type from './src/core/Type.js';
+import * as PrimitiveTypes from './src/type/PrimitiveTypes.js';
 
-export { Component };
+export { Component, Entity, System, Type, PrimitiveTypes };
--- src/ObjectPool.js
+++
@@ -1,3 +0,0 @@
-class ObjectPool {
-	constructor() {}
-}
