543b7cfdbeecdd3e489aae74e0e0702f1141296b
Noman5237 <anonyman637@gmail.com>
2022-01-29 00:16:41 +0600

    component pool working with 0(1) algo

--- test/unit/core/ComponentPool.test.js
+++ test/unit/core/ComponentPool.test.js
@@ -7,14 +7,12 @@ import InterfaceError from '../../../src/error/Interface.error.js';
 describe('Component Pool', () => {
 	const position = new Position();
 
-	describe('Instantiation', () => {
-		const positionPool = new ComponentPool(Position, 50);
-	});
+	describe('Instantiation', () => {});
 
 	describe('Implementation', () => {
 		describe('malloc', () => {
-			test('malloc returning correct indexes after succesful allocation', () => {
-				const positionPool = new ComponentPool(Position, 50);
+			test('malloc returning correct indexes after successful allocation', () => {
+				const positionPool = new ComponentPool({ type: Position, size: 50 });
 				expect(positionPool.malloc(position)).to.equals(0);
 				expect(positionPool.malloc(position)).to.equals(1);
 				expect(positionPool.malloc(position)).to.equals(2);
@@ -22,26 +20,45 @@ describe('Component Pool', () => {
 			});
 
 			test('throwing interface error when the pool is full', () => {
-				const positionPool = new ComponentPool(Position, 3);
+				const positionPool = new ComponentPool({ type: Position, size: 3 });
+
 				expect(() => {
 					positionPool.malloc(position);
 					positionPool.malloc(position);
 					positionPool.malloc(position);
 					positionPool.malloc(position);
-				}).to.throw(InterfaceError, 'Poolsize reached limits');
+				}).to.throw(InterfaceError, 'pool size limit reached');
 			});
 		});
+
 		describe('free', () => {
-			test('removing a component from component pool', () => {
-				const positionPool = new ComponentPool(Position, 3);
+			test('freeing a component from component pool', () => {
+				const positionPool = new ComponentPool({ type: Position, size: 8 });
 				positionPool.malloc(position);
 				positionPool.malloc(position);
 				positionPool.malloc(position);
-				expect(positionPool.totalUsed).to.equals(3);
+				positionPool.malloc(position);
+				positionPool.malloc(position);
+				positionPool.malloc(position);
+				positionPool.malloc(position);
 				positionPool.free(1);
-				expect(positionPool.totalFree).to.equals(1);
 				positionPool.free(2);
-				expect(positionPool.totalFree).to.equals(2);
+				expect(positionPool.malloc(position)).to.equals(2);
+				expect(positionPool.malloc(position)).to.equals(1);
+				expect(positionPool.malloc(position)).to.equals(7);
+				positionPool.free(2);
+			});
+		});
+
+		describe('componentAt', () => {
+			test('get component at free index', () => {
+				const positionPool = new ComponentPool({ type: Position, size: 8 });
+				positionPool.malloc(position);
+				positionPool.malloc(position);
+				positionPool.free(1);
+				expect(() => {
+					positionPool.componentAt(1);
+				}).to.throw(InterfaceError, 'memory at asked index is marked as free');
 			});
 		});
 	});
