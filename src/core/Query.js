70ba724f7c830d872cc17665014af192250602c8
Noman5237 <anonyman637@gmail.com>
2022-04-19 21:46:24 +0600

    fixing key of result

--- src/core/Query.js
+++ src/core/Query.js
@@ -5,15 +5,20 @@ class Query {
 		this.#dml = dml;
 	}
 
-	*run({ components, entities }) {
-			const mustHave = this.#dml.components.and;
-			entities.map((entityId) => {
-				const entity = Object.fromEntries(mustHave.map((type) => [type, components[type].getComponent(entityId)]));
-				const isEligible = Object.values(entity).filter((value) => value !== undefined).length === mustHave.length;
-				if(isEligible) {
-					yield entity;
-				}
-			});
+	run({ components, entities }) {
+		const mustHave = this.#dml.components.and;
+		const refinedEntities = [];
+		entities.forEach((entityId) => {
+			const entity = Object.fromEntries(
+				mustHave.map((type) => [type.name, components[type.name].getComponent(entityId)])
+			);
+			const isEligible = Object.values(entity).filter((value) => value !== undefined).length === mustHave.length;
+			if (isEligible) {
+				refinedEntities.push(entity);
+			}
+		});
+
+		return refinedEntities;
 	}
 }
 
